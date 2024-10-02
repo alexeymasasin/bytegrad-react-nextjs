62bb7b07de54fa6ada3ce247935dba88d9490af9
Alexey Masasin <alexeymasasin@gmail.com>
2024-10-02 22:18:52 +0500

    Split up the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Card from './Card';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <main>
-      <div>
-        <h1>Fancy Counter</h1>
-        <p>0</p>
-        <button>Reset</button>
-        <div>
-          <button>-</button>
-          <button>+</button>
-        </div>
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<Card />
+		</main>
+	);
 }
 
 export default App;
