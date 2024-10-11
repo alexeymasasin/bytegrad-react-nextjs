0e17a2c0d52751a76ae1c07b6985312941e51f29
Alexey Masasin <alexeymasasin@gmail.com>
2024-10-11 08:31:44 +0500

    Spacebar to increment

--- fancy-counter/src/Card.jsx
+++ fancy-counter/src/Card.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ButtonContainer from './ButtonContainer';
 import Count from './Count';
 import ResetButton from './ResetButton';
@@ -7,6 +7,23 @@ import Title from './Title';
 export default function Card() {
 	const [count, setCount] = useState(0);
 	const locked = count === 5 ? true : false;
+	useEffect(() => {
+		const handleKeydown = (e) => {
+			if (e.code === 'Space') {
+				const newCount = count + 1;
+				if (newCount > 5) {
+					setCount(5);
+					return;
+				}
+				setCount(newCount);
+			}
+		};
+		window.addEventListener('keydown', handleKeydown);
+
+		return () => {
+			window.removeEventListener('keydown', handleKeydown);
+		};
+	}, [count]);
 
 	return (
 		<div className={`card ${locked ? 'card--limit' : ''}`}>
--- fancy-counter/src/CountButton.jsx
+++ fancy-counter/src/CountButton.jsx
@@ -1,7 +1,7 @@
 import { MinusIcon, PlusIcon } from '@radix-ui/react-icons';
 
 export default function CountButton({ setCount, type, locked }) {
-	const handleClick = () => {
+	const handleClick = (e) => {
 		setCount((prev) => {
 			if (type === 'minus') {
 				const newCount = prev - 1;
@@ -17,6 +17,8 @@ export default function CountButton({ setCount, type, locked }) {
 				return newCount;
 			}
 		});
+
+		e.currentTarget.blur();
 	};
 
 	return (
