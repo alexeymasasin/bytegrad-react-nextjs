700acbc0f353d957963a00f3041b362d63eecc54
Alexey Masasin <alexeymasasin@gmail.com>
2024-10-12 08:02:05 +0500

    Folder structure update

--- word-analytics/src/components/Container.jsx
+++ word-analytics/src/components/Container.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
+import { FB_MAX_CHARS, IG_MAX_CHARS } from '../lib/constants';
 import Stats from './Stats';
 import Textarea from './Textarea';
-import { FB_MAX_CHARS, IG_MAX_CHARS } from './constants';
 
 export default function Container() {
 	const [text, setText] = useState('');
