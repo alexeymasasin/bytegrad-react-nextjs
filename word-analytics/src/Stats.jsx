c099558cc1f375529acc3f7b7eae296686719beb
Alexey Masasin <alexeymasasin@gmail.com>
2024-10-12 07:21:33 +0500

    Stats refactor

--- word-analytics/src/Stats.jsx
+++ word-analytics/src/Stats.jsx
@@ -1,22 +1,19 @@
 export default function Stats() {
 	return (
 		<section className="stats">
-			<section className="stat">
-				<span className="stat__number">0</span>
-				<h2 className="second-heading">Words</h2>
-			</section>
-			<section className="stat">
-				<span className="stat__number">0</span>
-				<h2 className="second-heading">Characters</h2>
-			</section>
-			<section className="stat">
-				<span className="stat__number">280</span>
-				<h2 className="second-heading">Instagram</h2>
-			</section>
-			<section className="stat">
-				<span className="stat__number">2200</span>
-				<h2 className="second-heading">Facebook</h2>
-			</section>
+			<Stat statNumber={0} statName="words" />
+			<Stat statNumber={0} statName="characters" />
+			<Stat statNumber={280} statName="instagram" />
+			<Stat statNumber={2200} statName="facebook" />
+		</section>
+	);
+}
+
+function Stat({ statNumber, statName }) {
+	return (
+		<section className="stat">
+			<span className="stat__number">{statNumber}</span>
+			<h2 className="second-heading">{statName}</h2>
 		</section>
 	);
 }
