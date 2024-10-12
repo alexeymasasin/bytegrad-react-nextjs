62ce345ed093499c26735b7a12ec8364d5970faf
Alexey Masasin <alexeymasasin@gmail.com>
2024-10-12 06:39:02 +0500

    Controlled textarea

--- word-analytics/src/Textarea.jsx
+++ word-analytics/src/Textarea.jsx
@@ -1,3 +1,16 @@
+import { useState } from 'react';
+
 export default function Textarea() {
-	return <textarea className="textarea" />;
+	const [text, setText] = useState('');
+	return (
+		<textarea
+			placeholder="Enter your text..."
+			spellCheck="false"
+			value={text}
+			onChange={(e) => {
+				setText(e.target.value);
+			}}
+			className="textarea"
+		/>
+	);
 }
