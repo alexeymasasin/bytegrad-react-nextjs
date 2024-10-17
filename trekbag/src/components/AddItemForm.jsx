702fbcde0f022031449ccdd29bcd4bc36b5ca414
Alexey Masasin <alexeymasasin@gmail.com>
2024-10-17 15:01:42 +0500

    Basic validation & autofocus

--- trekbag/src/components/AddItemForm.jsx
+++ trekbag/src/components/AddItemForm.jsx
@@ -1,30 +1,40 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Button from './Button';
 
 export default function AddItemForm({ setItems }) {
 	const [inputValue, setInputValue] = useState('');
+	const inputRef = useRef();
 
 	return (
 		<form
 			onSubmit={(e) => {
 				e.preventDefault();
+
 				const newItem = {
 					name: inputValue,
 					packed: false,
 					id: new Date().getTime(),
 				};
 
+				if (!inputValue) {
+					inputRef.current.focus();
+					return;
+				}
+
 				setItems((prev) => {
 					return [...prev, newItem];
 				});
 				setInputValue('');
+				inputRef.current.focus();
 			}}
 		>
 			<h2>Add an item</h2>
 			<input
+				ref={inputRef}
 				type="text"
 				value={inputValue}
 				onChange={(e) => setInputValue(e.target.value)}
+				autoFocus
 			/>
 			<Button type="primary" text="Add to list" />
 		</form>
