decbce8097951804275b9e5905b4c5cd46b6d4ae
Alexey Masasin <alexeymasasin@gmail.com>
2024-10-17 15:05:12 +0500

    AddItemForm refactor

--- trekbag/src/components/AddItemForm.jsx
+++ trekbag/src/components/AddItemForm.jsx
@@ -5,33 +5,33 @@ export default function AddItemForm({ setItems }) {
 	const [inputValue, setInputValue] = useState('');
 	const inputRef = useRef();
 
+	const submitHandler = (e) => {
+		e.preventDefault();
+
+		const newItem = {
+			name: inputValue,
+			packed: false,
+			id: new Date().getTime(),
+		};
+
+		if (!inputValue) {
+			inputRef.current.focus();
+			return;
+		}
+
+		setItems((prev) => {
+			return [...prev, newItem];
+		});
+		setInputValue('');
+		inputRef.current.focus();
+	};
+
 	return (
-		<form
-			onSubmit={(e) => {
-				e.preventDefault();
-
-				const newItem = {
-					name: inputValue,
-					packed: false,
-					id: new Date().getTime(),
-				};
-
-				if (!inputValue) {
-					inputRef.current.focus();
-					return;
-				}
-
-				setItems((prev) => {
-					return [...prev, newItem];
-				});
-				setInputValue('');
-				inputRef.current.focus();
-			}}
-		>
+		<form onSubmit={submitHandler}>
 			<h2>Add an item</h2>
 			<input
-				ref={inputRef}
 				type="text"
+				ref={inputRef}
 				value={inputValue}
 				onChange={(e) => setInputValue(e.target.value)}
 				autoFocus
