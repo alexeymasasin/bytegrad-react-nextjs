001ed121b53ec9034bbf22b56b841d5dc3311e83
Alexey Masasin <alexeymasasin@gmail.com>
2024-10-20 19:27:47 +0500

    Select boilerplate (79, 4:20)

--- trekbag/src/components/ItemList.jsx
+++ trekbag/src/components/ItemList.jsx
@@ -1,9 +1,17 @@
+import Select from 'react-select';
 import EmptyView from './EmptyView';
 
 export default function ItemList({ items, deleteItem, toggleItem }) {
 	return (
 		<ul className="item-list">
 			{items.length === 0 && <EmptyView />}
+
+			{items.length > 0 ? (
+				<section className="sorting">
+					<Select />
+				</section>
+			) : null}
+
 			{items.map((item) => (
 				<Item
 					toggleItem={toggleItem}
