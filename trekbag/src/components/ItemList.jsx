ccbf1e2f7b80b8059dbbff2f2f101c8406a6c14a
Alexey Masasin <alexeymasasin@gmail.com>
2024-10-21 20:08:11 +0500

    Sorting feature

--- trekbag/src/components/ItemList.jsx
+++ trekbag/src/components/ItemList.jsx
@@ -1,18 +1,49 @@
+import { useState } from 'react';
 import Select from 'react-select';
 import EmptyView from './EmptyView';
 
+const sortingOptions = [
+	{
+		label: 'Sort by default',
+		value: 'default',
+	},
+	{
+		label: 'Sort by packed',
+		value: 'packed',
+	},
+	{
+		label: 'Sort by unpacked',
+		value: 'unpacked',
+	},
+];
+
 export default function ItemList({ items, deleteItem, toggleItem }) {
+	const [sortBy, setSortBy] = useState('default');
+	const sortedItems = [...items].sort((a, b) => {
+		if (sortBy == 'packed') {
+			return b.packed - a.packed;
+		}
+		if (sortBy == 'unpacked') {
+			return a.packed - b.packed;
+		}
+		return;
+	});
+
 	return (
 		<ul className="item-list">
 			{items.length === 0 && <EmptyView />}
 
 			{items.length > 0 ? (
 				<section className="sorting">
-					<Select />
+					<Select
+						onChange={(option) => setSortBy(option.value)}
+						defaultValue={sortingOptions[0]}
+						options={sortingOptions}
+					/>
 				</section>
 			) : null}
 
-			{items.map((item) => (
+			{sortedItems.map((item) => (
 				<Item
 					toggleItem={toggleItem}
 					deleteItem={deleteItem}
