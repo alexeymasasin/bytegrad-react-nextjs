33aa8e8b3b538b2c9e7baa710074f9c3bd2c3293
Alexey Masasin <alexeymasasin@gmail.com>
2024-10-25 14:27:09 +0500

    Feedback yard boilerplate

---
+++ feedback-yard/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+	return (
+		<footer className="footer">
+			<small>
+				<p>
+					&copy; Copyright by{' '}
+					<a target="_blank" href="https://github.com/alexeymasasin">
+						@alexeymasasin
+					</a>
+				</p>
+				<p>All rights reserved</p>
+			</small>
+		</footer>
+	);
+}
