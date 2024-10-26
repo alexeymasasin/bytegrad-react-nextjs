e41d5d44cfebd45c1ac104d2ff2b6550f68a7fa7
Alexey Masasin <alexeymasasin@gmail.com>
2024-10-26 19:08:46 +0500

    Fetching feedbacks

--- feedback-yard/src/components/FeedbackList.tsx
+++ feedback-yard/src/components/FeedbackList.tsx
@@ -1,27 +1,25 @@
+import { useEffect, useState } from 'react';
 import FeedbackItem from './FeedbackItem';
 
-const feedbackItems = [
-	{
-		upvoteCount: 593,
-		badgeLetter: 'A',
-		companyName: 'AlexeyMasaisn',
-		text: 'test test test',
-		daysAgo: 4,
-	},
-	{
-		upvoteCount: 321,
-		badgeLetter: 'N',
-		companyName: 'Nike',
-		text: 'bla bla bla',
-		daysAgo: 4,
-	},
-];
-
 export default function FeedbackList() {
+	const [feedbackItems, setFeedbackItems] = useState([]);
+
+	useEffect(() => {
+		fetch(
+			'https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks'
+		)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				setFeedbackItems(data.feedbacks);
+			});
+	}, []);
+
 	return (
 		<ol className="feedback-list">
 			{feedbackItems.map((feedbackItem) => (
-				<FeedbackItem feedbackItem={feedbackItem} />
+				<FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
 			))}
 		</ol>
 	);
