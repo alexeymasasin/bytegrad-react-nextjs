127ca49604f30a6435adf66bd63aa033edc3e663
Alexey Masasin <alexeymasasin@gmail.com>
2024-10-26 17:51:14 +0500

    Feedback item component, mapping

--- feedback-yard/src/components/FeedbackList.tsx
+++ feedback-yard/src/components/FeedbackList.tsx
@@ -1,27 +1,35 @@
-import { TriangleUpIcon } from '@radix-ui/react-icons';
+import FeedbackItem from './FeedbackItem';
+
+const feedbackItems = [
+	{
+		upvoteCount: 593,
+		badgeLetter: 'A',
+		companyName: 'AlexeyMasaisn',
+		text: 'test test test',
+		daysAgo: 4,
+	},
+	{
+		upvoteCount: 321,
+		badgeLetter: 'N',
+		companyName: 'Nike',
+		text: 'bla bla bla',
+		daysAgo: 4,
+	},
+	{
+		upvoteCount: 1239,
+		badgeLetter: 'A',
+		companyName: 'Adidas',
+		text: 'test test test',
+		daysAgo: 3,
+	},
+];
 
 export default function FeedbackList() {
 	return (
 		<ol className="feedback-list">
-			<li className="feedback">
-				<button>
-					<TriangleUpIcon />
-					<span>593</span>
-				</button>
-				<div>
-					<p>B</p>
-				</div>
-
-				<div>
-					<p>AlexeyMasasin</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum neque
-						repellendus velit quibusdam ut quam?
-					</p>
-				</div>
-
-				<p>4d</p>
-			</li>
+			{feedbackItems.map((feedbackItem) => (
+				<FeedbackItem feedbackItem={feedbackItem} />
+			))}
 		</ol>
 	);
 }
